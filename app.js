5707a669ee09429874a77f3eb30555c01244969a
nov05 <you@example.com>
2026-08-05 03:02:53 -0500

    .

--- app.js
+++ app.js
@@ -12,27 +12,6 @@ fetch("data/tabs.json")
     });
 
 function displaySongs(list) {
-    let links = "";
-    if (song.colab) {
-        links += `
-        <a class="icon-link"
-           href="${song.colab}"
-           target="_blank"
-           onclick="event.stopPropagation();">
-            <img src="assets/20260805_logo_colab.png" alt="Colab">
-        </a>
-    `;
-    }
-    if (song.youtube) {
-        links += `
-        <a class="icon-link"
-           href="${song.youtube}"
-           target="_blank"
-           onclick="event.stopPropagation();">
-            <img src="assets/20260805_logo_youtube.png" alt="YouTube">
-        </a>
-    `;
-    }
     songBox.innerHTML = "";
     if (list.length === 0) {
         songBox.innerHTML =
@@ -40,22 +19,41 @@ function displaySongs(list) {
         return;
     }
     list.forEach(song => {
+        let links = "";
+        if (song.colab) {
+            links += `
+            <a class="icon-link"
+            href="${song.colab}"
+            target="_blank"
+            onclick="event.stopPropagation();">
+                <img src="assets/20260805_logo_colab.png" alt="Colab">
+            </a>
+            `;
+        }
+        if (song.youtube) {
+            links += `
+            <a class="icon-link"
+            href="${song.youtube}"
+            target="_blank"
+            onclick="event.stopPropagation();">
+                <img src="assets/20260805_logo_youtube.png" alt="YouTube">
+            </a>
+            `;
+        }
         let tags = "";
         song.tags.forEach(t => {
-            tags +=
-                `
-<span class="tag">${t}</span>
-`;
+            tags += `
+            <span class="tag">${t}</span>
+            `;
         });
-        songBox.innerHTML +=
-            `
-<div class="card"onclick="openPDF('${song.pdf}')">
-    <h3>${song.title}</h3>
-    <p>${song.artist}</p>
-    <div>${tags}</div>
-    <div class="links">${links}</div>
-</div>
-`;
+        songBox.innerHTML += `
+        <div class="card"onclick="openPDF('${song.pdf}')">
+            <h3>${song.title}</h3>
+            <p>${song.artist}</p>
+            <div>${tags}</div>
+            <div class="links">${links}</div>
+        </div>
+        `;
     });
 }
 
